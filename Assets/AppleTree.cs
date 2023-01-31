bd282170e78c6ae74f98ac85c4078e116024f12a
FennellChris <122921407+FennellChris@users.noreply.github.com>
2023-01-31 11:48:30 -0500

    Refined AppleTree Movement

--- Assets/AppleTree.cs
+++ Assets/AppleTree.cs
@@ -6,7 +6,7 @@ public class AppleTree : MonoBehaviour
 {
     [Header("Inscribed")]
 
-    public gameObject applePrefab;
+    public GameObject applePrefab;
 
     public float speed = 1f;
 
@@ -15,7 +15,7 @@ public class AppleTree : MonoBehaviour
     public float changeDirChance = 0.1f;
 
     public float appleDropDelay = 1f; 
-    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +25,24 @@ public class AppleTree : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+     Vector3 pos = transform.position;
+     pos.x += speed * Time.deltaTime;
+     transform.position = pos;
+
+     if (pos.x < -leftAndRightEdge){
+        speed = Mathf.Abs(speed);
+     }
+        else if (pos.x > leftAndRightEdge){
+            speed = -Mathf.Abs(speed);
+
+       // }else if ( Random.value < changeDirChance){
+         //   speed *= -1;
+        }
     }
+    void FixedUpdate(){
+        if (Random.value < changeDirChance){
+            speed *= -1;
+        }
+    }
+
 }
