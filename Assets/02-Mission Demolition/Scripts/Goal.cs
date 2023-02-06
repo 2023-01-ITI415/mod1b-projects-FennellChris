126b84f4e5af81715e1ccb38397ef9527b90c9fd
FennellChris <122921407+FennellChris@users.noreply.github.com>
2023-02-06 17:14:23 -0500

    Got lost

--- Assets/02-Mission Demolition/Scripts/Goal.cs
+++ Assets/02-Mission Demolition/Scripts/Goal.cs
@@ -2,15 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent ( typeof(Renderer))]
-public class Goal : MonoBehaviour {
+[RequireComponent (typeof(Renderer))]
+public class Goal : MonoBehaviour{
 	static public bool 	goalMet = false;
 
 	void OnTriggerEnter(Collider other) {
 		// when the trigger is hit by something
 		// check to see if it's a Projectile 
 		Projectile proj = other.GetComponent<Projectile>();
-		if ( proj != null) {
+		if (proj != null) {
 			// if so, set goalMet = true
 			Goal.goalMet = true;
 
@@ -22,5 +22,4 @@ public class Goal : MonoBehaviour {
 		}
 	}
     // Start is called before the first frame update
-	
 }
--- Assets/02-Mission Demolition/Slingshot.cs
+++ Assets/02-Mission Demolition/Slingshot.cs
@@ -56,9 +56,11 @@ void Update() {
         projRB.isKinematic = false;
         projRB.collisionDetectionMode = CollisionDetectionMode.Continuous;
         projRB.velocity = -mouseDelta * velocityMult;
+        FollowCam.SWITCH_VIEW( FollowCam.eView.slingshot );
         FollowCam.POI = projectile;
         Instantiate<GameObject>(projLinePrefab, projectile.transform);
         projectile = null;
+        MissionDemolition.SHOT_FIRED();
     }
 }
 }
