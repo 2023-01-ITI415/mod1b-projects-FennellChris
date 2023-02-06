1a393d47519819f4761349d93248daafb1058f67
FennellChris <122921407+FennellChris@users.noreply.github.com>
2023-02-06 10:57:17 -0500

    First bit of projectile istantiation

--- Assets/02-Mission Demolition/Slingshot.cs
+++ Assets/02-Mission Demolition/Slingshot.cs
@@ -3,12 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Slingshot : MonoBehaviour {
+    [Header("Inscribed")]
+    public GameObject projectilePrefab;
+    
+    [Header("Dynamic")]
     public GameObject launchPoint;
+    public Vector3 launchPos;
+    public GameObject projectile;
+    public bool aimingMode;
 
     void Awake () {
         Transform launchPointTrans = transform.Find("LaunchPoint");
         launchPoint = launchPointTrans.gameObject;
         launchPoint.SetActive( false );
+        launchPos = launchPointTrans.position;
     }
 void OnMouseEnter () {
     //print("Slingshot:OnMouseEnter()");
@@ -17,5 +25,11 @@ void OnMouseEnter () {
 void OnMouseExit () {
     //print("Slingshot:OnMouseExit");
     launchPoint.SetActive( false );
+}
+void OnMouseDown() {
+    aimingMode = true;
+    projectile = Instantiate( projectilePrefab ) as GameObject;
+    projectile.transform.position = launchPos;
+    projectile.GetComponent<Rigidbody>().isKinematic = true;
 }
 }
